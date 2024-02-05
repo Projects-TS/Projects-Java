6a220ce70f520a6cc9e1a7e1563b507020ab5951
talshoham <142199060+tal-rock@users.noreply.github.com>
2024-02-05 19:38:39 +0200

    Initial commit

---
+++ src/MyFirstTask.java
@@ -0,0 +1,14 @@
+public class MyFirstTask {
+    public static void main(String[] args) {
+
+        int age = 42;
+        double old = 3;
+
+        String first = "Hello, World!";
+        String second = "Goodbye, World!";
+        System.out.println(age);
+        System.out.println(old);
+        System.out.println(first);
+        System.out.println(second);
+    }
+}
