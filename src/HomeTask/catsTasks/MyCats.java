d7569545fc75c2ca74f7b3880574ce41145a4565
talshoham <142199060+tal-rock@users.noreply.github.com>
2024-03-15 12:26:55 +0200

    Merge remote-tracking branch 'origin/main'

--- src/HomeTask/catsTasks/MyCats.java
+++ src/HomeTask/catsTasks/MyCats.java
@@ -13,17 +13,32 @@ public class MyCats {
         cat1.setName("Ryan");
         cat1.setColor("blue");
         cat1.setAge(1);
+
         Cat cat2 = new Cat();
         cat2.setName("James");
         cat2.setColor("red");
         cat2.setAge(8);
+
         Cat cat3 = new Cat();
         cat3.setName("diana");
         cat3.setColor("green");
         cat3.setAge(14);
+
+        Cat cat4 = new Cat();
+        cat4.setName("Riki");
+        cat4.setColor("yellow");
+        cat4.setAge(11);
+
+        Cat cat5 = new Cat();
+        cat5.setName("John");
+        cat5.setColor("black");
+        cat5.setAge(7);
+
         catsList.add(cat1);
         catsList.add(cat2);
         catsList.add(cat3);
+        catsList.add(cat4);
+        catsList.add(cat5);
         printCatInformation(catsList);
     }
     private static void printCatInformation(List<Cat> catsList) {
