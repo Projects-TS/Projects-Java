7d1736925ba93252de73d99b7ab1c0d3c20b9f5d
talshoham <142199060+tal-rock@users.noreply.github.com>
2024-03-17 11:33:44 +0200

    Merge remote-tracking branch 'origin/main'

--- src/FinalExercises/Exercise_2.java
+++ src/FinalExercises/Exercise_2.java
@@ -1,7 +1,6 @@
 package FinalExercises;
 
 //this exercise is for checking the input year he was born and how old he will be 20 years later.
-import java.util.Calendar;
 import java.util.Scanner;
 
 public class Exercise_2 {
@@ -11,8 +10,6 @@ public class Exercise_2 {
         int age = Input.nextInt();
         int ageIn20Years = age + 20;
         int currentYear = 2024 - age;
-        int year = Calendar.getInstance().get(Calendar.YEAR);
-
         System.out.println("That means you were born in " + currentYear);
         System.out.println("That means that in 20 years you will be " + ageIn20Years + " years old!");
     }
